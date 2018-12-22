7badce5adfeb6ec44b22b5acb92838fffe7c5db4
Emmanuel Ajiboye <eoa22@njit.edu>
2018-12-22 03:34:22 -0500

    Task:added user Question migrate, model and made user seeder and added tests for user profile(cleanup), and question tesst

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+use Faker\Generator as Faker;
+
+
+
+$factory->define(App\Question::class, function (Faker $faker)
+{
+    return [
+        'body'=>$faker->paragraph($nbSentences = 3, $variablelengthSentences = true),
+    ];
+});
